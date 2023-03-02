7442a42c4d994d89f200938d3c15f90457037c6e
Zach Wilson <68037788+PlatFormPlayZ@users.noreply.github.com>
2023-03-01 19:52:41 -0600

    codehs js gaphics start

--- scripts/canvasHandler.js
+++ scripts/canvasHandler.js
@@ -1,6 +1,25 @@
 window.onload = function() {
+    var diagramImg = "../images/AB_arrow_right.png";
+    var width;
+    var height;
+    var centerWidth = width /2;
+    var centerHeight = height /2;
+
     // Your code goes here
-    
+    function start(){
+        width = getWidth();
+        height = getHeight();
+        var img = new WebImage("../images/AB_arrow_right.png");
+        println(test);
+        //var ratio =  img.height / img.width;
+        var ratio = 1;
+        img.setSize(width, width * ratio);
+        img.setPosition(0, 0);
+    }
+
+    function sendMessage(imgSrc, message){
+
+    }
 
     //End of Program
     if (typeof start === 'function') {
