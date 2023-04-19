7f336b41a452b7f2ea641c64b35b11fab5129657
Gabe Roy <74325925+GabeRoy01@users.noreply.github.com>
2023-04-18 19:25:36 -0500

    Documentation-Gabe~4-18-23
    Added documentation for animation-helper.js and encoding-helper.js

--- scripts/animation-helper.js
+++ scripts/animation-helper.js
@@ -1,3 +1,23 @@
+/*
+ * Filename: animation-helper.js
+ * Documented by: Gabe Roy
+ *
+ * Description:
+ * This file contains functions to change images and display styles
+ * in a step-by-step animation sequence, involving a sender, packet,
+ * and recipient.
+ */
+
+/**
+ * Change the sender image and reveal the next step in the animation.
+ *
+ * Changes the image for the sender to a new image and sets the display
+ * property of the element with the "step2" ID to "all" to reveal the
+ * next step in the animation.
+ *
+ * @returns {boolean} false is to prevent default behavior.
+ */
+
 function changeSenderImage()
 {
 var img = document.getElementById("senderimage");
@@ -7,6 +27,16 @@ document.getElementById("step2").style = "display:all";
 
 return false;
 }
+
+/**
+ * Change the packet image and update the visibility of animation steps.
+ *
+ * Changes the image for the packet to a new image, hides the current step
+ * with the "step2" ID, and reveals the next step with the "step3" ID in
+ * the animation.
+ *
+ * @returns {boolean} false is to prevent default behavior.
+ */
 
 function changePacketImage()
 {
@@ -20,6 +50,16 @@ document.getElementById("step3").style = "display:all";
 return false;
 }
 
+/**
+ * Change the recipient image and hide the current step in the animation.
+ *
+ * Changes the image for the recipient to a new image and sets the display
+ * property of the element with the "step3" ID to "none" to hide the
+ * current step in the animation.
+ *
+ * @returns {boolean} false is to prevent default behavior.
+ */
+
 function changeRecipientImage()
 {
 var img = document.getElementById("recipientimage");
@@ -29,5 +69,3 @@ document.getElementById("step3").style = "display:none";
 
 return false;
 }
- 
-   
--- scripts/encoding-helper.js
+++ scripts/encoding-helper.js
@@ -1,3 +1,21 @@
+/*
+ * Filename: encoding-helper.js
+ * Documented by: Gabe Roy
+ *
+ * Description:
+ * This file contains utility functions for converting between different
+ * encoding formats, such as PEM, Base64, and ArrayBuffer. These functions
+ * are helpful when working with encryption and encoding tasks.
+ */
+
+/**
+ * Converts a PEM-formatted string to a Base64 string.
+ *
+ * @param {string} value - The PEM-formatted string.
+ * @param {string} label - The label for the PEM format (e.g., "PRIVATE KEY").
+ * @returns {string} The resulting Base64 string.
+ */
+
 var pemToBase64String = function (value, label) {
     var lines = value.split("\n");
     var base64String = "";
@@ -10,6 +28,13 @@ var pemToBase64String = function (value, label) {
     return base64String;
 };
 
+/**
+ * Converts a Base64 string to an ArrayBuffer.
+ *
+ * @param {string} value - The Base64 string.
+ * @returns {ArrayBuffer} The resulting ArrayBuffer.
+ */
+
 var base64StringToArrayBuffer = function (value) {
     var byteString = atob(value);
     var byteArray = new Uint8Array(byteString.length); 
@@ -20,6 +45,14 @@ var base64StringToArrayBuffer = function (value) {
 
     return byteArray.buffer;
 };
+
+/**
+ * Converts a Base64 string to a PEM-formatted string.
+ *
+ * @param {string} value - The Base64 string.
+ * @param {string} label - The label for the PEM format (e.g., "PRIVATE KEY").
+ * @returns {string} The resulting PEM-formatted string.
+ */
 
 var base64StringToPem = function (value, label) {
     var pem = "-----BEGIN {0}-----\n".replace("{0}", label);
@@ -33,6 +66,13 @@ var base64StringToPem = function (value, label) {
     return pem;
 };
 
+/**
+ * Converts an ArrayBuffer to a Base64 string.
+ *
+ * @param {ArrayBuffer} value - The ArrayBuffer.
+ * @returns {string} The resulting Base64 string.
+ */
+
 var arrayBufferToBase64String = function (value) {
     var byteArray = new Uint8Array(value);
     var byteString = "";
@@ -44,9 +84,24 @@ var arrayBufferToBase64String = function (value) {
     return btoa(byteString);
 };
 
+/**
+ * Converts a PEM-formatted string to an ArrayBuffer.
+ *
+ * @param {string} value - The PEM-formatted string.
+ * @returns {ArrayBuffer} The resulting ArrayBuffer.
+ */
+
 var pemToArrayBuffer = function (value) {
     return base64StringToArrayBuffer(pemToBase64String(value));
 };
+
+/**
+ * Converts an ArrayBuffer to a PEM-formatted string.
+ *
+ * @param {ArrayBuffer} value - The ArrayBuffer.
+ * @param {string} label - The label for the PEM format (e.g., "PRIVATE KEY").
+ * @returns {string} The resulting PEM-formatted string.
+ */
 
 var arrayBufferToPem = function (value, label) {
     return base64StringToPem(arrayBufferToBase64String(value), label);
